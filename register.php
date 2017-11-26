31560ac6ac800f284322e019c0f7257963c50dc7
Álvaro <alburg@hotmail.es>
2017-11-26 10:30:57 +0100

    Últimos retoques

--- register.php
+++ register.php
@@ -3,9 +3,70 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-   holaaa
+  <header>
+        <div class="wrapper">
+            <div class="cabecera">
+               
+                <div class="logotipo">
+                    <a href="indexPrivado.html"><img src="img/logo.png" alt="logo"></a>
+                </div>
+                <!--poner en subir.html el nombre de la página-->
+                <div class="subir">
+                    <a href="upload_product.html"><img src="img/subir.png" alt="subir"></a>
+                </div>
+                
+                <div class="usuario">
+                   <!--poner en perfil.html el nombre de la página-->
+                    <a href="ownProfile.html"><img src="img/usuario.png" alt="usuario"></a>
+                </div>
+            </div>
+            <div class="buscador centrar">
+                <div>
+                    <form action="resultadoBusqueda.html" method="get">
+                        <input type="text" placeholder="buscar">
+                        <input type="button" value="Buscar">
+                    </form>
+                </div>
+                
+            </div>
+        </div>
+    </header>
+
+   <main class="centrar">
+       <h1>Bienvenido a ChangeAll</h1>
    <a href="indexPrivado.html">Ya puedes acceder</a> 
+   </main>
+   <footer>
+        <div class="wrapper">
+            <div class="pie">
+               <div class="rrss">
+                    <a href="https://www.facebook.com/"><img src="img/fb.png" alt="fb"></a>
+                    <a href="https://www.instagram.com/?hl=es"><img src="img/instagram.png" alt="inst"></a>
+                </div>
+                <div class="datosEmpresa">
+                   
+                        <a href="politicaLegal.html">
+                            <p>Política Legal</p>
+                        </a>   
+  
+                        <a href="Privacidad.html">
+                            <p>Privacidad</p>
+                        </a>
+                         <a href="politicaCookies.html">
+                            <p>Política de Cookies</p>
+                        </a>
+                    
+                        <a href="quienesSomos.html">
+                            <p>Quiénes Somos</p>
+                        </a> 
+                      
+                </div>
+            
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
